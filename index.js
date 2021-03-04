2aee03007bab900ecfe8693baa909717269c4632
Lina Deen <linadeen@Linas-MacBook-Pro.local>
2021-03-03 20:08:05 -0800

    3rd commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-//const session = require("express-session");
+const session = require("express-session");
 const bcrypt = require("bcrypt");
 const mysql = require("mysql");
 
